fc235143dafce1ae6430545db582de28b4186288
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-03-24 22:49:34 +0100

    feat(resource): add `withResource`

---
+++ libs/ngrx-toolkit/src/lib/with-resource.spec.ts
@@ -0,0 +1,225 @@
+import {
+  patchState,
+  signalStore,
+  withMethods,
+  withProps,
+  withState,
+} from '@ngrx/signals';
+import { reloadResource, withResource } from './with-resource';
+
+import { inject, Injectable, resource } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { httpResource, provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+
+type Address = {
+  street: string;
+  streetNumber: string;
+  city: {
+    zip: string;
+    district: string;
+    name: string;
+  };
+  country: string;
+};
+
+const venice: Address = {
+  street: 'Main Street',
+  streetNumber: '1A',
+  city: {
+    zip: '710D',
+    district: 'San Juliano',
+    name: 'Venice',
+  },
+  country: 'Italy',
+};
+
+@Injectable({ providedIn: 'root' })
+export class AddressResolver {
+  lookup(input: string) {
+    return Promise.resolve(venice);
+  }
+}
+
+describe('withResource', () => {
+  const setup = () => {
+    const resolver = TestBed.inject(AddressResolver);
+    const lookupSpy = jest.spyOn(resolver, 'lookup');
+    const Store = signalStore(
+      { providedIn: 'root' },
+      withState({ input: '', user: 'hi' }),
+      withProps(() => ({
+        _addressResolver: inject(AddressResolver),
+      })),
+      withResource((store) =>
+        resource({
+          request: () => store.input() || undefined,
+          loader: () => store._addressResolver.lookup(store.input()),
+        })
+      ),
+      withMethods((store) => ({
+        setInput: (input: string) => patchState(store, { input }),
+        reloadResource: () => {
+          reloadResource(store);
+        },
+      }))
+    );
+
+    return { store: TestBed.inject(Store), lookupSpy };
+  };
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(async () => {
+    await jest.runAllTimersAsync();
+    jest.useRealTimers();
+  });
+
+  it('should match the type of readonly resource', () => {
+    const Store = signalStore(
+      { providedIn: 'root' },
+      withResource(() =>
+        resource({
+          loader: () => Promise.resolve(venice),
+        })
+      )
+    );
+    const _addressResource = TestBed.runInInjectionContext(() =>
+      resource({
+        request: undefined,
+        loader: () => Promise.resolve(venice),
+      }).asReadonly()
+    );
+    const store = TestBed.inject(Store);
+
+    type AddressResource = typeof _addressResource;
+    const _store = store satisfies AddressResource;
+  });
+
+  it('should match the type of readonly resource with default value', () => {
+    const Store = signalStore(
+      { providedIn: 'root' },
+      withResource(() =>
+        resource({
+          defaultValue: venice,
+          loader: () => Promise.resolve(venice),
+        })
+      )
+    );
+    const _addressResource = TestBed.runInInjectionContext(() =>
+      resource({
+        request: undefined,
+        defaultValue: venice,
+        loader: () => Promise.resolve(venice),
+      }).asReadonly()
+    );
+    const store = TestBed.inject(Store);
+
+    type AddressResource = typeof _addressResource;
+    const _store = store satisfies AddressResource;
+  });
+
+  it('should initially not load the data', async () => {
+    const { store } = setup();
+
+    expect(store.hasValue()).toBe(false);
+    await jest.runAllTimersAsync();
+    expect(store.hasValue()).toBe(false);
+  });
+
+  it('should load the data on valid input', async () => {
+    const { store } = setup();
+
+    store.setInput('Domgasse 5');
+    await jest.runAllTimersAsync();
+    expect(store.hasValue()).toBe(true);
+    expect(store.value()).toBe(venice);
+  });
+
+  it('should go to error mode', async () => {
+    const { store, lookupSpy } = setup();
+    lookupSpy.mockRejectedValueOnce('offline');
+
+    store.setInput('Domgasse 5');
+    await jest.runAllTimersAsync();
+    expect(store.hasValue()).toBe(false);
+    expect(store.error()).toBe('offline');
+    expect(store.value()).toBe(undefined);
+  });
+
+  it('should throw if reload is called from the outside', () => {
+    const { store } = setup();
+    expect(() => store.reload()).toThrow('not implemented');
+  });
+
+  it('should reload on request change', async () => {
+    const newVenice = { ...venice };
+    const { store, lookupSpy } = setup();
+    lookupSpy.mockImplementation((input) =>
+      Promise.resolve(input === 'Domgasse 5' ? venice : newVenice)
+    );
+
+    store.setInput('Domgasse 5');
+    await jest.runAllTimersAsync();
+    expect(store.value()).toBe(venice);
+
+    store.setInput('Domgasse 6');
+    await jest.runAllTimersAsync();
+    expect(store.value()).toBe(newVenice);
+  });
+
+  it('should reload on reload call', async () => {
+    const { store, lookupSpy } = setup();
+    lookupSpy.mockResolvedValue(venice);
+
+    store.setInput('Domgasse 5');
+    await jest.runAllTimersAsync();
+    expect(store.value()).toBe(venice);
+
+    store.reloadResource();
+    await jest.runAllTimersAsync();
+    expect(store.value()).toBe(venice);
+    expect(lookupSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should rerun after error', async () => {
+    const { store, lookupSpy } = setup();
+    lookupSpy.mockRejectedValueOnce('offline');
+
+    store.setInput('Domgasse 5');
+    await jest.runAllTimersAsync();
+    expect(store.hasValue()).toBe(false);
+
+    lookupSpy.mockResolvedValueOnce(venice);
+    store.reloadResource();
+    await jest.runAllTimersAsync();
+    expect(store.hasValue()).toBe(true);
+    expect(store.value()).toBe(venice);
+  });
+
+  it('should also work with httpResource', async () => {
+    const Store = signalStore(
+      { providedIn: 'root' },
+      withResource(() => {
+        return httpResource(() => `/api/geo`);
+      })
+    );
+
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+
+    const store = TestBed.inject(Store);
+    const ctrl = TestBed.inject(HttpTestingController);
+    await jest.runAllTimersAsync();
+    ctrl.expectOne('/api/geo').flush(venice);
+    await jest.runAllTimersAsync();
+
+    expect(store.value()).toBe(venice);
+  });
+});
--- libs/ngrx-toolkit/src/lib/with-resource.ts
+++ libs/ngrx-toolkit/src/lib/with-resource.ts
@@ -1,5 +1,16 @@
-import { SignalStoreFeature, StateSignals } from '@ngrx/signals';
+import {
+  EmptyFeatureResult,
+  SignalStoreFeature,
+  StateSignals,
+} from '@ngrx/signals';
 import { SignalStoreFeatureResult } from '@ngrx/signals/src/signal-store-models';
+import {
+  Resource,
+  ResourceRef,
+  ResourceStatus,
+  Signal,
+  untracked,
+} from '@angular/core';
 
 type StoreForResource<Input extends SignalStoreFeatureResult> = StateSignals<
   Input['state']
@@ -7,12 +18,41 @@ type StoreForResource<Input extends SignalStoreFeatureResult> = StateSignals<
   Input['props'] &
   Input['methods'];
 
+type ResourceFeature<T> = EmptyFeatureResult & {
+  props: {
+    value: Signal<T>;
+    status: Signal<ResourceStatus>;
+    error: Signal<unknown>;
+    isLoading: Signal<boolean>;
+    __resource: {
+      [RESOURCE]: ResourceRef<T>;
+    };
+  };
+  methods: {
+    hasValue(): this is Resource<NonNullable<T>>;
+    reload(): boolean;
+  };
+};
+
+type NamedResourceFeature<
+  Name extends string,
+  ResourceValue
+> = EmptyFeatureResult & {
+  props: {
+    [Key in Name]: ResourceRef<ResourceValue>;
+  } & {
+    __resources: {
+      [RESOURCE]: Record<string, ResourceRef<ResourceValue>>;
+    };
+  };
+};
+
 /**
  *
  * Adds a resource which is directly integrated into
  * the SignalStore, making the instance fully type-compatible
  * with a ResourceRef, i.e. a readonly resource.
- * You can also call the "SignalStore as resource"-pattern.
+ * You can also call it the "SignalStore as resource"-pattern.
  *
  * That makes the SignalStore quite versatile, because it can be used
  * for all the upcoming APIs in Angular that require a resource and at
@@ -40,25 +80,198 @@ type StoreForResource<Input extends SignalStoreFeatureResult> = StateSignals<
  * And for `withHttpResource, following functions can be applied additionally:
  * - destroyHttpResource(store)
  *
+ * In a sense, it is not experimental, since the experimental resources
+ * are coming outside from Angular itself. We just provide the API.
+ *
+ * TODO: own resource type with more options (Marko's RFC)
+ * TODO: named resources
+ * TODO: Throw error on name collision
+ *
  * If a resource httpResource or resource already exists, we get a type error.
+ */
+export function withResource<Input extends SignalStoreFeatureResult, Value>(
+  resourceFactory: (store: StoreForResource<Input>) => ResourceRef<Value>
+): SignalStoreFeature<Input, ResourceFeature<Value>>;
+
+/**
+ * The named resource version creates resources at a property of the resourceName.
+ *
+ * The functions `reloadResource` and `setResourceValue` also accept a name.
+ *
+ * Putting it as a property is to be able to provide multipe `ResourceRef`
+ * types. Splitting up the various properties of `ResourceRef` with a named
+ * prefix would not allow that.
+ *
+ * Example:
+ * ```ts
+ * const Store = signalStore(
+ *   withState({
+ *     selectedId: undefined as number | undefined,
+ *   }),
+ *   withResource('list', store => withHttpResource<Product[]>(() => `/product`)),
+ *   withResource('detail', store => withHttpResource<ProductDetail>(() => `/product/${store.id()}`)),
+ * )
+ *
+ * const store = inject(Store);
+ *
+ * const listResource: ResourceRef<Product[]> = store.list;
+ * const detailResource: ResourceRef<ProductDetail[]> = store.detail;
+ *
+ * ```
+ * @param name name of the property where the resource is stored
+ * @param resourceFactory function generating the actual resource
  */
 export function withResource<
   Input extends SignalStoreFeatureResult,
-  Output extends SignalStoreFeatureResult
+  ResourceValue,
+  Name extends string
+>(
+  name: Name, // name should be seen first.
+  resourceFactory: (
+    store: StoreForResource<Input>
+  ) => ResourceRef<ResourceValue>
+): SignalStoreFeature<Input, NamedResourceFeature<Name, ResourceValue>>;
+
+/**
+ * Implementation Note
+ *
+ * We want to hide the resource from both inside (other SignalStore features)
+ * and the outside (component, services, etc.)
+ *
+ * Since we can't change the type of the SignalStore, that it automatically
+ * hides a RESOURCE symbol (like STATE_SOURCE), we hide the resource in the
+ * state behind __resource which encapsulates it for the consumer.
+ * Additionally, we put the actual resource into the property __resource
+ * into a RESOURCE symbol, so that it is also encapsulated for the features
+ * of the SignalStore.
+ */
+export function withResource<
+  Input extends SignalStoreFeatureResult,
+  ResourceValue,
+  Name extends string
+>(
+  nameOrResourceFactory:
+    | string
+    | ((store: StoreForResource<Input>) => ResourceRef<ResourceValue>),
+  resourceFactory?: (
+    store: StoreForResource<Input>
+  ) => ResourceRef<ResourceValue>
+): SignalStoreFeature<
+  Input,
+  NamedResourceFeature<Name, ResourceValue> | ResourceFeature<ResourceValue>
+> {
+  if (typeof nameOrResourceFactory === 'string') {
+    throw new Error('');
+  }
+
+  return createResourceFeature(nameOrResourceFactory);
+}
+
+function createResourceFeature<
+  Input extends SignalStoreFeatureResult,
+  ResourceValue
 >(
   resourceFactory: (
     store: StoreForResource<Input>
-  ) => SignalStoreFeature<Input, Output>
-): SignalStoreFeature<Input, Output> {
+  ) => ResourceRef<ResourceValue>
+): SignalStoreFeature<Input, ResourceFeature<ResourceValue>> {
   return (store) => {
-    const storeForFactory = {
+    const storeForResourceFactory = {
       ...store['stateSignals'],
       ...store['props'],
       ...store['methods'],
-    } as StoreForResource<Input>;
+    };
 
-    const feature = factoryFn(storeForFactory);
+    const resource = resourceFactory(storeForResourceFactory);
 
-    return feature(store);
+    return {
+      ...store,
+      props: {
+        ...store.props,
+        value: resource.value,
+        status: resource.status,
+        error: resource.error,
+        isLoading: resource.isLoading,
+        __resource: {
+          [RESOURCE]: resource,
+        },
+      },
+      methods: {
+        ...store.methods,
+        hasValue: (): this is Resource<NonNullable<ResourceValue>> => {
+          return resource.hasValue();
+        },
+        reload: (): boolean => {
+          throw new Error('not implemented');
+        },
+      },
+    };
   };
+}
+
+function createNamedResourceFeature<
+  Input extends SignalStoreFeatureResult,
+  ResourceValue,
+  Name extends string
+>(
+  name: Name,
+  resourceFactory: (
+    store: StoreForResource<Input>
+  ) => ResourceRef<ResourceValue>
+): SignalStoreFeature<Input, SignalStoreFeatureResult> {
+  return (store) => {
+    const storeForResourceFactory = {
+      ...store['stateSignals'],
+      ...store['props'],
+      ...store['methods'],
+    };
+
+    const resource = resourceFactory(storeForResourceFactory);
+    const existingResources =
+      '__resources' in store.props ? store.props['__resources'] : {};
+
+    return {
+      ...store,
+      props: {
+        ...store.props,
+        [name]: resource.asReadonly(),
+        __resources: {
+          ...existingResources,
+          [name]: resource,
+        },
+      },
+    };
+  };
+}
+
+const RESOURCE = Symbol('RESOURCE');
+
+type SignalStoreResource<T> = {
+  [RESOURCE]: ResourceRef<T>;
+};
+
+type ResourceState<T> = {
+  __resource: SignalStoreResource<T>;
+};
+
+/**
+ * sets the value of the internal resource.
+ * @param value
+ */
+export function setResource<T>(value: T) {
+  return (state: ResourceState<T>) => {
+    const resource = state.__resource[RESOURCE];
+    resource.set(value);
+    return state;
+  };
+}
+
+/**
+ * Triggers the `reload` method on the internal resource.
+ * @param store
+ */
+export function reloadResource(store: ResourceState<unknown>) {
+  untracked(() => {
+    store.__resource[RESOURCE].reload();
+  });
 }
