0d3165c82a9fb316824b95da2a4a01d2b1db78b6
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2025-04-05 13:34:52 +0200

    fix: withResource

--- libs/ngrx-toolkit/src/index.ts
+++ libs/ngrx-toolkit/src/index.ts
@@ -1,28 +1,26 @@
-import { withResource } from './lib/with-resource';
-
+export { withDisabledNameIndices } from './lib/devtools/features/with-disabled-name-indicies';
+export { withGlitchTracking } from './lib/devtools/features/with-glitch-tracking';
+export { withMapper } from './lib/devtools/features/with-mapper';
+export { renameDevtoolsName } from './lib/devtools/rename-devtools-name';
+export { patchState, updateState } from './lib/devtools/update-state';
 export { withDevToolsStub } from './lib/devtools/with-dev-tools-stub';
 export { withDevtools } from './lib/devtools/with-devtools';
-export { withDisabledNameIndices } from './lib/devtools/features/with-disabled-name-indicies';
-export { withMapper } from './lib/devtools/features/with-mapper';
-export { withGlitchTracking } from './lib/devtools/features/with-glitch-tracking';
-export { patchState, updateState } from './lib/devtools/update-state';
-export { renameDevtoolsName } from './lib/devtools/rename-devtools-name';
 
 export {
-  withRedux,
-  payload,
-  noPayload,
-  createReducer,
   createEffects,
+  createReducer,
+  noPayload,
+  payload,
+  withRedux,
 } from './lib/with-redux';
 
-export * from './lib/with-call-state';
-export * from './lib/with-undo-redo';
-export * from './lib/with-data-service';
-export { withStorageSync, SyncConfig } from './lib/with-storage-sync';
-export * from './lib/with-pagination';
-export { withReset, setResetState } from './lib/with-reset';
 export { withImmutableState } from './lib/immutable-state/with-immutable-state';
+export * from './lib/with-call-state';
+export { emptyFeature, withConditional } from './lib/with-conditional';
+export * from './lib/with-data-service';
 export { withFeatureFactory } from './lib/with-feature-factory';
-export { withConditional, emptyFeature } from './lib/with-conditional';
-export { withResource, setResource, reloadResource } from './lib/with-resource';
+export * from './lib/with-pagination';
+export { setResetState, withReset } from './lib/with-reset';
+export { reloadResource, setResource, withResource } from './lib/with-resource';
+export { SyncConfig, withStorageSync } from './lib/with-storage-sync';
+export * from './lib/with-undo-redo';
